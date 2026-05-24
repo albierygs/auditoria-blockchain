2590bb4333a691543bd444294e32183c3ba50c27
Albiery Gonçalves dos Santos <albierygoncalves2@gmail.com>
2026-05-24 16:55:56 -0300

    fix prisma output

--- packages/api/src/utils/db.js
+++ packages/api/src/utils/db.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require("../../generated/prisma");
+const { PrismaClient } = require("@prisma/client");
 const { DATABASE_URL } = require("./constants");
 
 const db = new PrismaClient({ datasourceUrl: DATABASE_URL });
